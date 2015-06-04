807bb56332827505c8ac8c91b60f6c377207f9a2
Mark Borcherding <markborcherding@gmail.com>
2015-06-03 21:08:03 -0500

    Collect callbacks to run before stopping, and after starting an image
    We need to tell our load balancers about these two events. We can
    provide callbacks that allow us to do so.

--- spec/deploy_dsl_spec.rb
+++ spec/deploy_dsl_spec.rb
@@ -111,4 +111,36 @@ describe Centurion::DeployDSL do
 
     expect(DeployDSLTest.get_current_tags_for('asdf')).to eq [ { server: 'host1', tags: [ 'foo'] } ]
   end
+
+  describe '#before_stopping_image' do
+    it 'collects before_stopping_image callbacks as procs' do
+      callback = ->(server) { }
+      DeployDSLTest.before_stopping_image callback
+      expect(DeployDSLTest.fetch(:before_stopping_image_callbacks)).to eq([callback])
+    end
+
+    it 'collects before_stopping_image callbacks as blocks' do
+      DeployDSLTest.before_stopping_image do |_|
+        'from the block'
+      end
+      callback = DeployDSLTest.fetch(:before_stopping_image_callbacks)[0]
+      expect(callback.call).to eq('from the block')
+    end
+  end
+
+  describe '#after_image_started' do
+    it 'collects after_image_started callbacks as procs' do
+      callback = ->(server) { }
+      DeployDSLTest.after_image_started callback
+      expect(DeployDSLTest.fetch(:after_image_started_callbacks)).to eq([callback])
+    end
+
+    it 'collects after_image_started callbacks as blocks' do
+      DeployDSLTest.after_image_started do |_|
+        'from the block'
+      end
+      callback = DeployDSLTest.fetch(:after_image_started_callbacks)[0]
+      expect(callback.call).to eq('from the block')
+    end
+  end
 end
